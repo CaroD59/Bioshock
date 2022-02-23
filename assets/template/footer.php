a82c7cac49a01af305b2e82de21b236416f2ecf0
CaroD59 <dorchies.c@gmail.com>
2022-02-23 13:15:43 +0100

    Add images

---
+++ assets/template/footer.php
@@ -0,0 +1,9 @@
+<head>
+  <link rel="stylesheet" href="assets/css/footer.css">
+</head>
+
+<body>
+  <footer>
+    <p class="footer">The images used are not free of rights. For any request to remove an image that you own, please contact the following email address: ramdom@contact.com</p>
+  </footer>
+</body>
